c234f7b02df00b1c3f19253ddd8e36f0f9afa852
Dhruv Senjaliya <113255723+senjaliyadhruv@users.noreply.github.com>
2025-06-26 18:51:52 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://ac6013847cb6f4be5bc25a16319b0dc3-2122672688.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "http://20.193.248.112";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
